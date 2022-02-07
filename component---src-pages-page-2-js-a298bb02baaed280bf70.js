b6691e4d9ffe009ea345bebad6a3f9e8e91bf2ef
gurpr337 <gurpr337@gmail.com>
2022-02-07 15:54:07 -0500

    Updates

--- component---src-pages-page-2-js-a298bb02baaed280bf70.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{p5nM:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n("Wbzz"),o=n("Bl7J"),c=n("vrFN");t.default=function(){return a.createElement(o.a,null,a.createElement(c.a,{title:"Page two"}),a.createElement("h1",null,"Hi from the second page"),a.createElement("p",null,"Welcome to page 2"),a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-a298bb02baaed280bf70.js.map
